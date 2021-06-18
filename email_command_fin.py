c3a75f2fec2ba472210fec25b2fbbc2c77474add
Pranay Wajjala <44583332+greenscizor@users.noreply.github.com>
2021-06-18 11:51:48 +0530

    Update email_command_fin.py

--- email_command_fin.py
+++ email_command_fin.py
@@ -1,12 +1,14 @@
+#created by Pranay Wajjala
+#greenscizor
+
 import os
 import subprocess
 from pathlib import Path
 
-txt = Path(r'C:\Users\prana\Documents\out.txt').read_text()
+txt = Path(r'path-to-output-text-file').read_text()
 txt = txt.replace('\n', '')
 if txt == "create folder":
-        path = 'C:\\Users\\prana\\Documents\\Email'
+        path = 'path-to-the-folder'
         os.chdir(path)
-        Newfolder = 'task_1'
+        Newfolder = 'your-folder-name'
         os.makedirs(Newfolder)
-
